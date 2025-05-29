25dd82d0ddd5ccc70a23c37912a39a6247f03bd7
JulianLipa <lipajulianp@gmail.com>
2025-05-28 23:34:44 -0300

    fixed bugs v2

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import Test from "./components/main/RoboFlowDetection/Test";
-import { LanguageSwitcher } from "./components/main/LanguageSwitcher/LanguageSwitcher";
 
 export default function Home() {
   return (
